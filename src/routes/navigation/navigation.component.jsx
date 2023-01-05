ef625582625b3071d61a4f7d1a94eb21ddfa548c
Sun_r <rithsenghorn@gmail.com>
2023-01-04 17:59:13 -0700

    update home to cat icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,14 +3,15 @@ import { Outlet, Link } from "react-router-dom";
 import './navigation.styles.scss'
 import myResume from '../../assets/Seng_Horn_Rith_Resume.pdf'
 import HomeIcon from '@mui/icons-material/Home';
-
+import logo from "../../assets/cat.ico";
+import { borderRadius } from "@mui/system";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <HomeIcon  style={{ fontSize: 48 }}/>
+          <img src={logo} alt="Logo" style={{width:70, height:70, borderRadius:20 }}/>
         </Link>
         <div class="container">
           <div class="neon">Seng</div>
